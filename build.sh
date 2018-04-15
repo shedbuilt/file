9801dc58a44b14699a8f358969401522e0980102
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:08:20 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
         SHEDPKG_PREFIX='/tools'
         ;;
@@ -7,6 +7,6 @@ case "$SHED_BUILDMODE" in
         SHEDPKG_PREFIX='/usr'
         ;;
 esac
-./configure --prefix=$SHEDPKG_PREFIX || return 1
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+./configure --prefix=$SHEDPKG_PREFIX &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
